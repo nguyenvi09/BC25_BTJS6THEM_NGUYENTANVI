d081a55c9419e26b6bce21afce996ad595c5129b
Nguyen <Tan-V.Nguyen@aia.com>
2022-03-24 10:18:47 +0700

    up scr

--- main.js
+++ main.js
@@ -8,21 +8,29 @@ function kiemTraSNT(n){
     for(i = 2; i <= Math.sqrt(n); i++){
         if(n % i == 0){
             return 0;
-        }
+        };
     };
     return 1;
 };
 
-$("tinhSoNguyenTo").onclick = function(){
-    var n = $("n").value *1;
-    var ketQua = "";
-
-    if(n >= 2){
-        console.log(n + " là số nguyên tố");
-    };
-    for(i = 3; i < n; i += 2){
-        if(kiemTraSNT(n) == 1){
-            console.log(ketQua += i);
+var ketQua = "";
+function inSoNguyenTo(n){
+    for(j = 1; j <= n; j++){
+        if(kiemTraSNT(j) == 1){
+            ketQua += j + " ";
         };
     };
+    return ketQua;
+};
+
+$("tinhSoNguyenTo").onclick = function(){
+    var n = $("n").value *1;
+
+    if(kiemTraSNT(n) == 1){
+       inSoNguyenTo(n);
+    }else{
+        ketQua = n + "không phải số nguyên tố";
+    };
+
+    $("ketQua").innerHTML = ketQua;
 };
